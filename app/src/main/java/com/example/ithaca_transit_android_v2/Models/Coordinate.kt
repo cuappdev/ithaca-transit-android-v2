a0537399f694cdc9e30fef026f5a226751284692
Justin Jiang <jiangj2620@gmail.com>
2019-10-26 10:33:59 -0400

    add brief comments to top of coordinate and loaction models

--- app/src/main/java/com/example/ithaca_transit_android_v2/Models/Coordinate.kt
+++ app/src/main/java/com/example/ithaca_transit_android_v2/Models/Coordinate.kt
@@ -1,6 +1,6 @@
 package com.example.ithaca_transit_android_v2.models
 
-//Coordinate.kt
+//Coordinate.kt - Represents the longitude and latitude of a Location
 data class Coordinate(
     val latitude: Double,
     val longitude: Double
--- app/src/main/java/com/example/ithaca_transit_android_v2/Models/Location.kt
+++ app/src/main/java/com/example/ithaca_transit_android_v2/Models/Location.kt
@@ -1,8 +1,8 @@
 package com.example.ithaca_transit_android_v2.models
 
-//Location.kt
+//Location. - Represents either a bus stop or a google place.
 data class Location (
     val name: String,
     val coordinate: Coordinate,
     val detail: String?
-) {}
+) {}    

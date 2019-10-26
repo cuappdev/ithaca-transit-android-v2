ac7e8675216b1bea67ce67868a3ae16fb12fba09
Haichen Wang <hw595@cornell.edu>
2019-10-26 10:23:57 -0400

    Added Trip.kt

---
+++ app/src/main/java/com/example/ithaca_transit_android_v2/Models/Trip.kt
@@ -0,0 +1,13 @@
+package com.example.ithaca_transit_android_v2.Models
+
+// [Trip] is a collection of [Route] objects and other essential information
+// to represent a way of getting to the destination
+data class Trip (
+    val routes: List<Route>,
+    val startLocation: Location,
+    val endLocation: Location,
+    val isWalkingOnly: Boolean,
+    val arrival: Date,
+    val depart: Date,
+    val boardInMin: Int
+) {}

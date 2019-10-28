9f5cf95d334a4478b70cdcc7314fe5f57456e916
Justin Jiang <jiangj2620@gmail.com>
2019-10-27 23:46:21 -0400

    create sealed parent search state class, make all sub-search state classes inherit from parent

--- app/src/main/java/com/example/ithaca_transit_android_v2/states/SearchState.kt
+++ app/src/main/java/com/example/ithaca_transit_android_v2/states/SearchState.kt
@@ -1,37 +1,38 @@
 package com.example.ithaca_transit_android_v2.states
 
+import com.example.ithaca_transit_android_v2.models.Coordinate
 import com.example.ithaca_transit_android_v2.models.Location
-import java.util.*
 
-//Default
-class SearchState
+//Parent Search State Class - Contains Default parameters
+sealed class SearchState(
+    val startLocation: Location = Location("", Coordinate(0.0,0.0),""),
+    val endLocation: Location = Location("", Coordinate(0.0,0.0),"") ,
+    val favDestinations: List<Location> = mutableListOf(),
+    val recentDestinations: List<Location> = mutableListOf()
+){}
 
 //Default UI when launching app. Just displays a search bar (no interactions so far)
-data class LaunchState(
-
-){}
+class LaunchState(
+): SearchState(){}
 
 //First time using the app, after clicking on the search bar, there will be nothing
 //but "Search For a Destination" text and a big magnifying glass
-data class EmptyInitSearchState(
-
-){}
+class EmptyInitSearchState(
+): SearchState(){}
 
 //Click on search bar -> Changes UI by dropping a list of Favorite Destinations and
 //recent destinations
-data class InitSearchState(
-    val favDestinations : List<Location>,
-    val recentDestinations : List<Location>
-){}
-
-
+class InitSearchState(
+    favDestinations : List<Location>,
+    recentDestinations : List<Location>
+): SearchState(favDestinations = favDestinations, recentDestinations = recentDestinations)
 
 //Immediately following InitSearchState when user begins typing. Requires network call
-data class InitNetworkState(
+class InitNetworkState(
     val  searchText : String,
     //Network call to all locations that match the search text
     val  searchedLocations : List<Location>
-){}
+): SearchState(){}
 
 // 2 Possible Ways to get to Route Display State
 // 1)
@@ -43,11 +44,10 @@ data class InitNetworkState(
 // Brings them to the Route Display State -
 // Start Destination name on Left -> Destination name on Right
 // Default start location is Current Location
-data class RouteDisplayState(
-    val startLocation: Location,
-    val endLocation: Location,
-    val date : Date
-){}
+class RouteDisplayState(
+    startLocation: Location,
+    endLocation: Location
+): SearchState(startLocation = startLocation, endLocation = endLocation){}
 
 //How to get to Route Options state
 //1)From Route Display State, click on either the start or end location names
@@ -55,34 +55,24 @@ data class RouteDisplayState(
 //Brings them to the Route Options Menu -
 // 2. A clickable bar with Start Location name - User can click it, but won't make any network calls
 // 3. A clickable bar with Destination name - User can click it, but won't make any network calls
-// 4. Underneath the Search will be populated by Favorite Destinations and Recents
-data class RouteOptionState(
-    val startLocation: Location,
-    val endLocation: Location,
-    val favDestinations : List<Location>,
-    val recentDestinations : List<Location>
+// 4. Underneath the Search will be populated by Favorite Destinations and Recent Destinations
+class RouteOptionState(
+    startLocation: Location,
+    endLocation: Location,
+    favDestinations : List<Location>,
+    recentDestinations : List<Location>
 
-
-){}
+): SearchState(startLocation,endLocation,favDestinations,recentDestinations){}
 
 //In the Route Options state, when the user clicks either the start or end location
 //It automatically creates a black typing field for the User to enter their new
 //desired start or end location name
 //NO Network requirment yet
-data class ChangeLocationState(
+class ChangeLocationState(
     //What the user inputs
-    val searchText : String,
+    searchText : String,
     //Network call to all locations that match the search text
-    val  searchedLocations : List<Location>
-){}
-
-
-//When the User clicks on the clock icon to specify a time they want to leave
-//Creates a pop up that allows user to choose a time for arrival or destination
-data class TimeState(
-    val leaveTime : Time,
-    val arriveTime: Time,
-    val latest : Boolean
-){}
+    searchedLocations : List<Location>
+): SearchState(){}
 
 

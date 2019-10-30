4fdf944f7b019f171aff6b869f43d7bc3a681ddb
as3484 <as3484@cornell.edu>
2019-10-29 22:27:26 -0400

    committing a modified MapState with nonnull Date Objects

--- app/src/main/java/com/example/ithaca_transit_android_v2/Models/MapState.kt
+++ app/src/main/java/com/example/ithaca_transit_android_v2/Models/MapState.kt
@@ -2,24 +2,25 @@ package com.example.ithaca_transit_android_v2.Models
 
 import com.example.ithaca_transit_android_v2.models.Coordinate
 import com.example.ithaca_transit_android_v2.models.Location
+import java.util.*
+
 sealed class MapState(
     val trip:Trip = Trip(
         emptyList(),
         Location("", Coordinate(0.0, 0.0), ""),
         Location("", Coordinate(0.0, 0.0), ""),
         false,
-        null,
-        null,
+        Date(0),
+        Date(0),
         0)
 ) {}
 
 class LaunchState():MapState()
-
 class LaunchStateFilled(
     busStops: List<Coordinate>
 ): MapState()
 
-class DestinationSearch():MapState() {}
+class BusStopsShown():MapState() {}
 
 class TripOptions(
     firstBusTrip: Trip,

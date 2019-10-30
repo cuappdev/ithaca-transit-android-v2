c5b8d455802e67dd728bb0b33b2c023ab52f2f53
Haichen Wang <hw595@cornell.edu>
2019-10-30 18:15:48 -0400

    Fix import statement in Trip and improve RouteCard

--- app/src/main/java/com/example/ithaca_transit_android_v2/states/RouteCard.kt
+++ app/src/main/java/com/example/ithaca_transit_android_v2/states/RouteCard.kt
@@ -1,89 +1,43 @@
 package com.example.ithaca_transit_android_v2.states
 
-import com.example.ithaca_transit_android_v2.Models.Route
+import com.example.ithaca_transit_android_v2.Models.Trip
 import com.example.ithaca_transit_android_v2.models.Coordinate
 import com.example.ithaca_transit_android_v2.models.Location
-import java.util.*
+import java.util.Date
 
 // Parent Class
 sealed class RouteCard (
-    val routes: List<Route> = emptyList(),
-    val startLocation: Location = Location("", Coordinate(0.0,0.0),""),
-    val endLocation: Location = Location("", Coordinate(0.0,0.0),""),
-    val isWalkingOnly: Boolean = false,
-    val arrival: Date = Date(0),
-    val depart: Date = Date(0),
-    val boardInMin: Int = 0
+    val trip: Trip = Trip(
+        emptyList(),
+        Location("", Coordinate(0.0,0.0),""),
+        Location("", Coordinate(0.0,0.0),""),
+        false,
+        Date(0),
+        Date(0),
+        0
+    )
 ){}
 
 /* Route cards at the bottom of launch screen
-   Bus number displayed is obtained from [routes]
-   Keeping all the parameters here to be safe,
-   but in practice some might not be needed
+ * Bus number displayed is obtained from [routes]
+ * Keeping all the parameters here to be safe,
+ * but in practice some might not be needed
 */
-class LaunchCardState(
-    routes: List<Route>,
-    startLocation: Location,
-    endLocation: Location,
-    isWalkingOnly: Boolean,
-    arrival: Date,
-    depart: Date,
-    boardInMin: Int
-): RouteCard(
-    routes = routes,
-    startLocation = startLocation,
-    endLocation = endLocation,
-    isWalkingOnly = isWalkingOnly,
-    arrival = arrival,
-    depart = depart,
-    boardInMin = boardInMin
-)
+class LaunchCardState(trip: Trip): RouteCard(trip = trip)
 
 // A route card at launch screen is clicked
 class LaunchCardClickedState(): RouteCard()
 
 // A default display of a route card in route option view
 // [isWalkingOnly] and number of [routes] affects the drawing of transit details
-class CardFoldedDisplayState(
-    routes: List<Route>,
-    startLocation: Location,
-    endLocation: Location,
-    isWalkingOnly: Boolean,
-    arrival: Date,
-    depart: Date,
-    boardInMin: Int
-): RouteCard(
-    routes = routes,
-    startLocation = startLocation,
-    endLocation = endLocation,
-    isWalkingOnly = isWalkingOnly,
-    arrival = arrival,
-    depart = depart,
-    boardInMin = boardInMin
-)
+class CardFoldedDisplayState(trip: Trip): RouteCard(trip = trip)
 
 // A route card at route option screen is clicked
 class OptionCardClickedState(): RouteCard()
 
 // [CardDetailDisplayState] is the state for the route detail view
 // Used when a card at either launch screen or route options is clicked
-class CardDetailDisplayState(
-    routes: List<Route>,
-    startLocation: Location,
-    endLocation: Location,
-    isWalkingOnly: Boolean,
-    arrival: Date,
-    depart: Date,
-    boardInMin: Int
-): RouteCard(
-    routes = routes,
-    startLocation = startLocation,
-    endLocation = endLocation,
-    isWalkingOnly = isWalkingOnly,
-    arrival = arrival,
-    depart = depart,
-    boardInMin = boardInMin
-)
+class CardDetailDisplayState(trip: Trip): RouteCard(trip = trip)
 
 // User clicks on the [Leave Now] button in route detail view and leave at dialog pops up
 class LeaveAtClickedState: RouteCard()

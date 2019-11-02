1b961641df7e5913eb760c687fa95486796c460c
Haichen Wang <hw595@cornell.edu>
2019-11-02 16:09:27 -0400

    Fix formatting of comments

--- app/src/main/java/com/example/ithaca_transit_android_v2/states/RouteCard.kt
+++ app/src/main/java/com/example/ithaca_transit_android_v2/states/RouteCard.kt
@@ -18,17 +18,19 @@ sealed class RouteCard (
     )
 ){}
 
-/* Route cards at the bottom of launch screen
+/*
+ * Route cards at the bottom of launch screen
  * Bus number displayed is obtained from [trip.routes]
  * Keeping all the parameters here to be safe,
  * but in practice some might not be needed
-*/
+ */
 class LaunchCardState(trip: Trip): RouteCard(trip = trip)
 
 // A route card at launch screen is clicked
 class LaunchCardClickedState(): RouteCard()
 
-/* A default display of a route card in route option view
+/*
+ * A default display of a route card in route option view
  * [trip.isWalkingOnly] and number of [trip.routes] affects the drawing of transit details
  */
 class CardFoldedDisplayState(trip: Trip): RouteCard(trip = trip)
@@ -36,7 +38,8 @@ class CardFoldedDisplayState(trip: Trip): RouteCard(trip = trip)
 // A route card at route option screen is clicked
 class OptionCardClickedState(): RouteCard()
 
-/* [CardDetailDisplayState] is the state for the route detail view
+/*
+ * [CardDetailDisplayState] is the state for the route detail view
  * Used when a card at either launch screen or route options is clicked
  */
 class CardDetailDisplayState(trip: Trip): RouteCard(trip = trip)

b907f28bec6d50bef38a5671311ecbb676adccc8
Lesley Huang <lh484@cornell.edu>
2019-11-02 17:30:27 -0400

    initial set up for future nova (#9)

---
+++ app/src/main/java/com/example/ithaca_transit_android_v2/NetworkUtils.kt
@@ -0,0 +1,12 @@
+package com.example.ithaca_transit_android_v2
+
+import com.appdev.futurenovajava.Endpoint;
+import com.appdev.futurenovajava.FutureNovaRequest;
+
+/*
+ * NetworkUtils include all the networking calls needed
+ */
+class NetworkUtils {
+
+
+}

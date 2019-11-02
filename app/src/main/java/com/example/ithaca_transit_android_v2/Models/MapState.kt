d05a42dd64fc72ad4b532e9060c2d0bfebfa909f
aasthashah999 <aasthashah999@gmail.com>
2019-11-02 18:01:23 -0400

    Committing a changed Route class and MapState class (#6)
    * commiting a changed Route class and MapState class
    
    * committing a modified MapState with nonnull Date Objects
    
    * redefining the parent class to include list of Trips. The subsequent state classes were updated.
    
    * In this commit, I removed a redunant state class which showed the busStops. I also added comments for each class
    
    * I changed busStops from Coordinate to Location

---
+++ app/src/main/java/com/example/ithaca_transit_android_v2/Models/MapState.kt
@@ -0,0 +1,28 @@
+package com.example.ithaca_transit_android_v2.Models
+
+import com.example.ithaca_transit_android_v2.models.Coordinate
+import com.example.ithaca_transit_android_v2.models.Location
+import java.util.Date
+
+sealed class MapState(
+    trips: List<Trip>
+)
+//State that represents the empty map before a network call has been
+class LaunchState():MapState(emptyList())
+
+//State that represents the map with all bus stops marked and marks the user's current location.
+class LaunchStateFilled(
+    busStops: List<Location>,
+    currentLocation: Coordinate
+): MapState(emptyList())
+
+//state that shows the first trip option and the walking trip.
+class TripOptions(
+    firstBusTrip: Trip,
+    walkingTrip: Trip
+): MapState(listOf(firstBusTrip, walkingTrip))
+
+//state that shows just selected path
+class SelectedTrip(
+    selectedTrip: Trip
+):MapState(listOf(selectedTrip))

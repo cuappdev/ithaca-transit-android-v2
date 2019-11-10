5d5c0e99e924642d02e1b135a96765dbee09745c
Lesley Huang <ningning621@gmail.com>
2019-11-10 16:15:12 -0500

    final changes

--- app/src/main/java/com/example/ithaca_transit_android_v2/models/LocationType.kt
+++ app/src/main/java/com/example/ithaca_transit_android_v2/models/LocationType.kt
@@ -3,6 +3,8 @@ package com.example.ithaca_transit_android_v2.models
 import com.squareup.moshi.Json
 
 enum class LocationType {
-    @Json(name = "busStop") BUS_STOP,
-    @Json(name="googlePlace") GOOGLE_PLACE
+    @Json(name = "busStop")
+    BUS_STOP,
+    @Json(name = "googlePlace")
+    GOOGLE_PLACE
 }

46c8610787950c67c61a4a07e3857c7255e71fb8
Justin Jiang <jiangj2620@gmail.com>
2019-11-10 15:27:14 -0500

    Remove extra parsing done on enums, remove extra @Json(name), change searchstate parameters

--- app/src/main/java/com/example/ithaca_transit_android_v2/MainActivity.kt
+++ app/src/main/java/com/example/ithaca_transit_android_v2/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         // TODO (lesley): just for testing purposes - replace with rxjava
         runBlocking {
             val deferred = CoroutineScope(Dispatchers.IO).async {
-                NetworkUtils().getSearchedLocations("balch")
+                NetworkUtils().getSearchedLocations("7-Eleven")
             }.await()
 
             Log.d("testing-final", deferred.toString())

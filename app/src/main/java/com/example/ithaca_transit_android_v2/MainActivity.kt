df45e18de92371b02cc4ef8c645ede1e9701c1bc
Lesley Huang <ningning621@gmail.com>
2019-11-10 04:31:45 -0500

    Revert "Finished networking"
    This reverts commit 3a4d2b797d7e09235cbef5255646a1762d4aca61.

--- app/src/main/java/com/example/ithaca_transit_android_v2/MainActivity.kt
+++ app/src/main/java/com/example/ithaca_transit_android_v2/MainActivity.kt
@@ -4,7 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.ithaca_transit_android_v2.networking.NetworkUtils
-import kotlinx.coroutines.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,15 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO (lesley): just for testing purposes - replace with rxjava
-        runBlocking {
-             val deferred = CoroutineScope(Dispatchers.IO).async {
-                NetworkUtils().getSearchedLocations("balch")
-            }.await()
-
-            Log.d("testing-final", deferred.toString())
-        }
-        //
-
+        val test = GlobalScope.async { NetworkUtils().getSearchedLocations("balch")}.getCompleted()
+        Log.d("testing", test.toString())
     }
 }

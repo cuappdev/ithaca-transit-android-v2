cb049b039151243b14418e9c434eb9cd7a075fcf
Haichen Wang <hw595@cornell.edu>
2019-11-23 17:52:07 -0500

    Removed an unnecessary comment

--- app/src/main/java/com/example/ithaca_transit_android_v2/models/Stop.kt
+++ app/src/main/java/com/example/ithaca_transit_android_v2/models/Stop.kt
@@ -1,7 +1,6 @@
 package com.example.ithaca_transit_android_v2.models
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-//Location. - Represents either a bus stop or a google place.
 @JsonClass(generateAdapter = true)
 // TODO: deprecate this class 
 data class Stop(

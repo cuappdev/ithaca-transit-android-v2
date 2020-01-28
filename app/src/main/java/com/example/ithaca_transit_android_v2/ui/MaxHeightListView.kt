7839ff4a56c445454707112417faf0583fa11ea0
Connor Reinhold <cyreinhold@gmail.com>
2020-01-28 11:56:13 -0500

    More commenting

--- app/src/main/java/com/example/ithaca_transit_android_v2/ui/MaxHeightListView.kt
+++ app/src/main/java/com/example/ithaca_transit_android_v2/ui/MaxHeightListView.kt
@@ -5,13 +5,18 @@ import android.util.AttributeSet
 import android.util.TypedValue
 import android.widget.ListView
 
-/* This class is a ListView that has a maximum height of 250 pixels. It is used by the Search View to
-* display a list of available options */
-class MaxHeightListView(context: Context, attributeSet: AttributeSet): ListView(context, attributeSet) {
+/* This class is a ListView that has a maximum height of 250 pixels. It is used by the main SearchView to
+* display a list of available options. We can't just wrap the ListView in some layout of constant height
+* because then the ListView would always have a constant height, we want it to expand up to a specific
+* height. */
+class MaxHeightListView(context: Context, attributeSet: AttributeSet) :
+    ListView(context, attributeSet) {
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         var maxHeightMeasureSpec = heightMeasureSpec
-        val maxHeight = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
-            250f, getResources().getDisplayMetrics())
+        val maxHeight = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            250f, getResources().getDisplayMetrics()
+        )
         maxHeightMeasureSpec = MeasureSpec.makeMeasureSpec(maxHeight.toInt(), MeasureSpec.AT_MOST)
         super.onMeasure(widthMeasureSpec, maxHeightMeasureSpec)
     }

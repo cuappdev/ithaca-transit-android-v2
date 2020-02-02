ee39d98fb14fc8c823a2ba77a23402c9181934b2
Lesley Huang <ningning621@gmail.com>
2020-02-02 13:50:14 -0500

    Added presenter class for search  (#32)
    * initial presenter
    
    * fixing small bugs
    
    * final bug fixes
    
    * removed change in gradle.build
    
    * added comments

--- app/src/main/java/com/example/ithaca_transit_android_v2/MainActivity.kt
+++ app/src/main/java/com/example/ithaca_transit_android_v2/MainActivity.kt
@@ -1,31 +1,24 @@
 package com.example.ithaca_transit_android_v2
 
-import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
-import android.view.View
-import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ithaca_transit_android_v2.models.Location
-import com.example.ithaca_transit_android_v2.states.*
+import com.example.ithaca_transit_android_v2.presenters.SearchPresenter
 import com.example.ithaca_transit_android_v2.ui_adapters.SearchViewAdapter
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import io.reactivex.Observable
-import io.reactivex.ObservableEmitter
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_toolbar_search.*
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-    private lateinit var disposable: Disposable
+    private lateinit var searchDisposable: Disposable
     private var mSearchLocations: List<Location> = ArrayList()
-    private var mSearchAdapter: SearchViewAdapter? = null
+    private lateinit var mSearchAdapter: SearchViewAdapter
+    private lateinit var mSearchPresenter: SearchPresenter
 
     override fun onMapReady(map: GoogleMap?) {
         map!!.setOnMapClickListener { point ->
@@ -34,105 +27,30 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun createSearchObservable(): Observable<SearchState> {
-        val obs = Observable.create { emitter: ObservableEmitter<SearchState> ->
-            val watcher: TextWatcher = object : TextWatcher {
-                override fun afterTextChanged(p0: Editable?) {
-                }
-
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                }
-
-                override fun onTextChanged(searchText: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    if (searchText!!.isEmpty()) {
-                        emitter.onNext(EmptyInitClickState())
-                    } else {
-                        emitter.onNext(InitSearchState(searchText.toString()))
-                    }
-                }
-            }
-
-            search_input.addTextChangedListener(watcher)
-
-            search_input.setOnFocusChangeListener { view, hasFocus ->
-                if (hasFocus && (view as EditText).text.isEmpty()) {
-                    // search input clicked on with no text
-                    emitter.onNext(EmptyInitClickState())
-                } else if (hasFocus) {
-                    // search input clicked on with text query
-                    emitter.onNext(InitSearchState((view as EditText).text.toString()))
-                } else {
-                    // search input not focused - display the un-expanded version
-                    emitter.onNext(SearchLaunchState())
-                }
-            }
-
-            emitter.setCancellable {
-                search_input.removeTextChangedListener(watcher)
-                search_input.setOnFocusChangeListener(null)
-            }
-        }
-        return obs.startWith(SearchLaunchState())
-    }
-
-    private fun initSearchView() {
-        val observable = createSearchObservable()
-
-        disposable = observable
-            .observeOn(Schedulers.io())
-            .map { state ->
-                if (state is InitSearchState) {
-                    val locations = NetworkUtils().getSearchedLocations(state.searchText)
-                    InitLocationsSearchState(state.searchText, locations)
-                } else {
-                    state
-                }
-            }
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ state ->
-                when (state) {
-                    is SearchLaunchState -> {
-                        search_empty_state.visibility = View.GONE
-                        search_locations_state.visibility = View.GONE
-                    }
-                    is EmptyInitClickState -> {
-                        search_locations_state.visibility = View.GONE
-                        search_empty_state.visibility = View.VISIBLE
-                    }
-                    is InitLocationsSearchState -> {
-                        search_empty_state.visibility = View.GONE
-                        search_locations_state.visibility = View.VISIBLE
-                        if (state.searchedLocations!!.size > 0 || search_input.text.isEmpty()) {
-                            mSearchAdapter!!.swapItems(state.searchedLocations)
-                        }
-                    }
-                }
-            }, { error -> Log.e("An Error Occurred", error.toString()) })
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         mSearchAdapter = SearchViewAdapter(this, mSearchLocations)
+        mSearchPresenter = SearchPresenter(search_view, this, mSearchAdapter)
+
+        searchDisposable = mSearchPresenter.initSearchView()
+        (map_fragment as SupportMapFragment).getMapAsync(this)
+
+        // set up search adapter
         locations_list.adapter = mSearchAdapter
         locations_list.setOnItemClickListener { parent, view, position, id ->
             val destination = parent.getItemAtPosition(position) as Location
+            // TODO: change map state to reflect destination
 
-            val intent = Intent(this, RouteOptionsActivity::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-
-
+            // for debugging purposes only
+            Toast.makeText(this, destination.name, Toast.LENGTH_SHORT).show()
         }
-        initSearchView()
-        (map_fragment as SupportMapFragment).getMapAsync(this)
     }
 
     override fun onStop() {
         super.onStop()
-        if (!disposable.isDisposed) {
-            disposable.dispose()
+        if (!searchDisposable.isDisposed) {
+            searchDisposable.dispose()
         }
     }
 }
---
+++ app/src/main/java/com/example/ithaca_transit_android_v2/presenters/SearchPresenter.kt
@@ -0,0 +1,109 @@
+package com.example.ithaca_transit_android_v2.presenters
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import com.example.ithaca_transit_android_v2.NetworkUtils
+import com.example.ithaca_transit_android_v2.models.Location
+import com.example.ithaca_transit_android_v2.states.*
+import com.example.ithaca_transit_android_v2.ui_adapters.SearchViewAdapter
+import io.reactivex.Observable
+import io.reactivex.ObservableEmitter
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_toolbar_search.view.*
+
+class SearchPresenter(_view: View, _context: Context, _searchAdapter: SearchViewAdapter) {
+    var view: View = _view
+    var context: Context = _context
+    private var mSearchLocations: List<Location> = ArrayList()
+    var mSearchAdapter: SearchViewAdapter = _searchAdapter
+
+    /**
+     * Create search observable object and emit states corresponding to changes in the search bar
+     */
+    private fun createSearchObservable(): Observable<SearchState> {
+        val obs = Observable.create { emitter: ObservableEmitter<SearchState> ->
+            val watcher: TextWatcher = object : TextWatcher {
+                override fun afterTextChanged(p0: Editable?) {
+                }
+
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun onTextChanged(searchText: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    if (searchText!!.isEmpty()) {
+                        emitter.onNext(EmptyInitClickState())
+                    } else {
+                        emitter.onNext(InitSearchState(searchText.toString()))
+                    }
+                }
+            }
+
+            view.search_input.addTextChangedListener(watcher)
+
+            view.search_input.setOnFocusChangeListener { view, hasFocus ->
+                if (hasFocus && (view as EditText).text.isEmpty()) {
+                    // search input clicked on with no text
+                    emitter.onNext(EmptyInitClickState())
+                } else if (hasFocus) {
+                    // search input clicked on with text query
+                    emitter.onNext(InitSearchState((view as EditText).text.toString()))
+                } else {
+                    // search input not focused - display the un-expanded version
+                    emitter.onNext(SearchLaunchState())
+                }
+            }
+
+            emitter.setCancellable {
+                view.search_input.removeTextChangedListener(watcher)
+                view.search_input.setOnFocusChangeListener(null)
+            }
+        }
+        return obs.startWith(SearchLaunchState())
+    }
+
+    /**
+     * Ran during onCreate() of the main activity to subscribe onto the state changes for search
+     */
+    fun initSearchView(): Disposable {
+        val observable = createSearchObservable()
+
+        return observable
+            .observeOn(Schedulers.io())
+            .map { state ->
+                if (state is InitSearchState) {
+                    mSearchLocations = NetworkUtils().getSearchedLocations(state.searchText) ?: ArrayList()
+                    InitLocationsSearchState(state.searchText, mSearchLocations)
+                } else {
+                    state
+                }
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ state ->
+                when (state) {
+                    is SearchLaunchState -> {
+                        view.search_empty_state.visibility = View.GONE
+                        view.search_locations_state.visibility = View.GONE
+                    }
+                    is EmptyInitClickState -> {
+                        view.search_locations_state.visibility = View.GONE
+                        view.search_empty_state.visibility = View.VISIBLE
+                    }
+                    is InitLocationsSearchState -> {
+                        view.search_empty_state.visibility = View.GONE
+                        view.search_locations_state.visibility = View.VISIBLE
+
+                        // update search results if the networking returns a new results list
+                        if (state.searchedLocations!!.isNotEmpty() || view.search_input.text.isEmpty()) {
+                            mSearchAdapter.swapItems(state.searchedLocations)
+                        }
+                    }
+                }
+            }, { error -> Log.e("An Error Occurred", error.toString()) })
+    }
+}

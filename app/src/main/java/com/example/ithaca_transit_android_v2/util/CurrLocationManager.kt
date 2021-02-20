b84e1dbb6fa8263c38cabecf39e8328bf111a6f0
Adam Kadhim <adamakadhim@gmail.com>
2021-02-19 22:19:06 -0500

    fixed location issue

--- app/src/main/java/com/example/ithaca_transit_android_v2/util/CurrLocationManager.kt
+++ app/src/main/java/com/example/ithaca_transit_android_v2/util/CurrLocationManager.kt
@@ -5,19 +5,26 @@ import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.LocationManager
+import android.os.Looper
+import android.util.Log
 import androidx.core.app.ActivityCompat
+import com.example.ithaca_transit_android_v2.Repository
+import com.google.android.gms.location.*
 
 // Manages the user's location and calls CurrLocationListener
 class CurrLocationManager(c: Context, locationManager: LocationManager, activity: Activity) {
     var mContext: Context
     var mActivity: Activity
     var mLocationManager: LocationManager
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     init {
         mContext = c
         mActivity = activity
         mLocationManager = locationManager
-        val locationListener = CurrLocationListener()
+        locationRequest = LocationRequest.create()
         if (ActivityCompat.checkSelfPermission(mContext,Manifest.permission.ACCESS_COARSE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED &&
             ActivityCompat.checkSelfPermission(
@@ -25,12 +32,16 @@ class CurrLocationManager(c: Context, locationManager: LocationManager, activity
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
         ) {
-            locationManager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER,
-                0,
-                0f,
-                locationListener
-            )
+            locationCallback = object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult?) {
+                    Repository.currentLocation = locationResult?.lastLocation
+                }
+            }
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(mActivity)
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper())
         } else {
             requestPermissions()
         }
